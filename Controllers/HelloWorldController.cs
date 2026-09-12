97421a7798cb54504a371ee7c4f6f3b76bf4c836
isaacpassnav <pasapera279@gmail.com>
2026-09-11 22:38:40 -0500

    week02 inital mvc. feat: controller has working

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers;
+
+public class HelloWorldController : Controller
+{
+    public string Index()
+    {
+        return "This is my default action";
+    }
+    public string Wellcome(string name, int numTimes = 1 )
+    {
+        return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is : {numTimes}");
+    }
+}
+
