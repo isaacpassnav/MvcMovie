027086e6cda14e81bdaf13fc6c889eed3194ca28
isaacpassnav <pasapera279@gmail.com>
2026-09-14 10:51:56 -0500

    Week02 working in core MVC ASP.NET

--- Program.cs
+++ Program.cs
@@ -1,6 +1,12 @@
+using MvcMovie.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<MvcMovieContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("MvcMovieContext")));
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
